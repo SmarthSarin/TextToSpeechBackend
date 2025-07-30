de13d54610e1a3845cfe3c009172d76e2f5c6e75
Sneha0907 <sneha09072001@gmail.com>
2025-07-30 21:11:43 +0530

    vercel url update

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://talk2text-mern.vercel.app"
+  "https://speech-to-text-frontend-phi.vercel.app"
 ];
 
 app.use(cors({
